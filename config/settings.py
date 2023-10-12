4db8895586ad0b04d14aa5dcba4a87578f110b57
KatyshaVtyrina <katyshavtyrina@gmail.com>
2023-10-12 21:40:45 +0300

    add database and static

--- config/settings.py
+++ config/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mailings',
+        'USER': 'postgres',
+        'PASSWORD': '1379',
     }
 }
 
@@ -117,6 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_FILE_DIRS = (
+    BASE_DIR / 'static',
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
