5504b7a14871c7ee5e8c090f9941677b581a9f42
KatyshaVtyrina <katyshavtyrina@gmail.com>
2023-10-30 19:24:48 +0300

    update

--- users/views.py
+++ users/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.shortcuts import redirect, get_object_or_404
+from django.urls import reverse_lazy, reverse
+from django.views.generic import CreateView, UpdateView
 
-# Create your views here.
+from users.services import send_code_email
+from users.forms import UserRegisterForm, UserProfileForm
+from users.models import User
+
+
+class RegisterView(CreateView):
+    model = User
+    form_class = UserRegisterForm
+    template_name = 'users/register.html'
+    success_url = reverse_lazy('users:login')
+
+    def form_valid(self, form):
+        obj = form.save()
+        obj.is_active = False
+        obj.save()
+        send_code_email(obj)
+        return super().form_valid(form)
+
+
+class ProfileView(UpdateView):
+    model = User
+    form_class = UserProfileForm
+    success_url = reverse_lazy('users:profile')
+
+    def get_object(self, queryset=None):
+        return self.request.user
+
+
+def verification_user(request, user_pk):
+    """Верификация почты, после прохождения по ссылке перенаправляет на личные данные"""
+    user = get_object_or_404(User, pk=user_pk)
+    user.is_active = True
+    user.save()
+    login(request, user)
+    return redirect(reverse('users:profile'))
