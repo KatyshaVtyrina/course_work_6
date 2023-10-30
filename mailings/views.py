b7a656f09166d3a9ee16bb0d6339b0f03969b186
KatyshaVtyrina <katyshavtyrina@gmail.com>
2023-10-30 19:41:21 +0300

    add register, profile

--- mailings/views.py
+++ mailings/views.py
@@ -2,12 +2,17 @@ from django.shortcuts import render
 
 from django.views.generic import ListView
 
-from mailings.models import Client
+from mailings.models import Client, Mailings
 
 
 def display_home(request):
     return render(request, 'mailings/home.html')
 
 
-# class ClientListView(ListView):
-#     model = Client
+class ClientListView(ListView):
+    model = Client
+
+
+class MailingsListView(ListView):
+    model = Mailings
+
