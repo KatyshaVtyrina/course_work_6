91da4598be59f3e2aaa313ca6bb90876b882efa0
KatyshaVtyrina <katyshavtyrina@gmail.com>
2023-10-30 18:37:47 +0300

    add User, rename Newsletter to Mailings

--- mailings/models.py
+++ mailings/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+from config import settings
 
 NULLABLE = {'blank': True, 'null': True}
 
@@ -11,6 +12,9 @@ class Client(models.Model):
     name = models.CharField(max_length=150, verbose_name='ФИО')
     comment = models.TextField(verbose_name='Комментарий', **NULLABLE)
 
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, verbose_name='Пользователь',
+                             **NULLABLE)
+
     class Meta:
         verbose_name = 'клиент'
         verbose_name_plural = 'клиенты'
@@ -19,8 +23,17 @@ class Client(models.Model):
         return f'{self.email} - {self.name}'
 
 
-class Newsletter(models.Model):
+class Mailings(models.Model):
     """Модель, описывающая настройки рассылки"""
     ...
 
+
+class Message(models.Model):
+    """Модель, описывающая тему и сообщение рассылки"""
+    ...
+
+
+class MailingsLogs(models.Model):
+    """Модель, описывающая логи рассылки"""
+    ...
 
