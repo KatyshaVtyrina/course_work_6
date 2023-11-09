3b2efc0b41b83141f4fcdbef2fafc153e63405c2
KatyshaVtyrina <katyshavtyrina@gmail.com>
2023-11-09 21:51:44 +0300

    add urls blog

--- config/urls.py
+++ config/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
 
     path('', include('mailings.urls', namespace='mailings')),
     path('users/', include('users.urls', namespace='users')),
+    path('blog/', include('blog.urls', namespace='blog')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
