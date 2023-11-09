bbe9578309b4a07f72499b9eb59fca1129bf500a
KatyshaVtyrina <katyshavtyrina@gmail.com>
2023-11-09 18:25:09 +0300

    add admin Post

--- blog/admin.py
+++ blog/admin.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 from blog.models import Post
 
 
-# @admin.register(Post)
-# class PostAdmin(admin.ModelAdmin):
-#     list_display = ('email', 'name', 'comment',)
-#     search_fields = ('name', 'email',)
-#     list_filter = ('name',)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'image', 'created_at', 'views_count',)
+    search_fields = ('title', 'created_at', )
+    list_filter = ('title', 'created_at', 'views_count',)
