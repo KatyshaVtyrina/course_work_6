ecb869a05eab2490e2183f36f2960e1480a1fbc2
KatyshaVtyrina <katyshavtyrina@gmail.com>
2023-11-09 21:55:33 +0300

    add ListView, Detail for Post

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+
+from blog.models import Post
+
+
+class PostListView(ListView):
+    model = Post
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+    def get_object(self, queryset=None):
+        post = super().get_object(queryset)
+        post.increase_views_count()
+        return post
