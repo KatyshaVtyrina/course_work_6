095f43ce4bcf510e9382772dfcf712bcc93d3877
KatyshaVtyrina <katyshavtyrina@gmail.com>
2023-11-12 11:05:37 +0300

    update form_valid in MailingCreateView (add task)

--- mailings/views.py
+++ mailings/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 from mailings.forms import ClientForm, MailingForm, MessageForm
-from mailings.models import Client, Mailings, Message
+from mailings.models import Client, Message
+from mailings.tasks import *
 
 
 def display_home(request):
@@ -65,10 +66,43 @@ class MailingCreateView(CreateView):
     success_url = reverse_lazy('mailings:mailing_list')
 
     def form_valid(self, form):
-        self.object = form.save()
-        self.object.user = self.request.user
-        self.object.save()
-        return super().form_valid(form)
+        if form.is_valid():
+            # создание рассылки
+            mailings = Mailings.objects.create(
+                time_start=form.cleaned_data['time_start'],
+                time_end=form.cleaned_data['time_end'],
+                frequency=form.cleaned_data['frequency'],
+                message=form.cleaned_data['message'],
+                user=self.request.user
+            )
+            # создание клиентов
+            mailings.clients.set(form.cleaned_data['clients'])
+
+            # парсим дату
+            date_start = parser.parse(str(form.cleaned_data['time_start']))
+
+            # если время старта в будущем, отправляем рассылку по времени старта
+            if date_start > timezone.now():
+                create_mailing.s(
+                    form.cleaned_data['time_start'],
+                    form.cleaned_data['time_end'],
+                    mailings.id,
+                    form.cleaned_data['message'].subject,
+                    form.cleaned_data['message'].body,
+                ).apply_async(eta=date_start)
+            # в ином случае отправляем сразу
+            else:
+                create_mailing.s(
+                    form.cleaned_data['time_start'],
+                    form.cleaned_data['time_end'],
+                    mailings.id,
+                    form.cleaned_data['message'].subject,
+                    form.cleaned_data['message'].body,
+                ).apply_async()
+
+            mailings.status = 'STARTED'
+            mailings.save()
+        return redirect('mailings:mailing_menu')
 
 
 class MailingUpdateView(UpdateView):
