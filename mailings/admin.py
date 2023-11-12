8841d338d72424e6ee5904af9ab74af926792237
KatyshaVtyrina <katyshavtyrina@gmail.com>
2023-11-12 10:58:40 +0300

    add MailingsLogs to admin

--- mailings/admin.py
+++ mailings/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from mailings.models import Client
+from mailings.models import Client, MailingsLogs
 
 
 @admin.register(Client)
@@ -8,3 +8,8 @@ class ClientAdmin(admin.ModelAdmin):
     list_display = ('email', 'name', 'comment',)
     search_fields = ('name', 'email',)
     list_filter = ('name',)
+
+
+@admin.register(MailingsLogs)
+class MailingsLogs(admin.ModelAdmin):
+    list_display = ('server_response', 'mailing', 'status', 'time')
